2c353d196009def13056224a27a5aa4d0c65e450
DarkCyberon <Xcom.D@yandex.ru>
2024-12-06 17:55:31 +0500

    UnitTestsAdd

--- src/test/java/com/example/demo/StudentServiceTests.java
+++ src/test/java/com/example/demo/StudentServiceTests.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import com.example.demo.core.StudentRepository;
 import com.example.demo.core.StudentService;
+import com.example.demo.exception.BadRequestException;
 import com.example.demo.exception.StudentNotFoundException;
+import com.example.demo.integration.BookingClient;
+import com.example.demo.integration.ChuckClient;
+import com.example.demo.model.ChuckResponse;
 import com.example.demo.model.Gender;
 import com.example.demo.model.Student;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
@@ -11,8 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,10 +27,14 @@ public class StudentServiceTests {
 
     @Autowired
     private StudentRepository studentRepository;
+    @Autowired
+    private ChuckClient chuckClient;
+    @Autowired
+    private BookingClient bookingClient;
 
     //Unit test
     @Test
-    public void returnExistedStudent() {
+    public void getReturnExistedStudent() {
         //arrange
         Long id = 1L;
         Student expectedStudent = new Student(id, "Gnome", "javaGnome@mail.com", Gender.OTHER, "", 0);
@@ -43,7 +51,7 @@ public class StudentServiceTests {
 
     //Unit test
     @Test
-    public void throwExceptionForNotExistedStudent()  {
+    public void throwExceptionForGetNotExistedStudent()  {
         //arrange
         Long id = 1L;
         studentRepository = mock(StudentRepository.class);
@@ -53,6 +61,76 @@ public class StudentServiceTests {
         StudentService studentService = new StudentService(studentRepository, null, null);
         //assert
         assertThrows(StudentNotFoundException.class, () -> studentService.getStudent(id));
+    }
+
+    //Unit test
+    @Test
+    public void addNewStudent() {
+        //arrange
+        String joke = "";
+        Long id = 1L;
+        Student newStudent = new Student(id, "Gnome", "javaGnome@mail.com", Gender.OTHER, "", 0);
+        chuckClient = mock(ChuckClient.class);
+        studentRepository = mock(StudentRepository.class);
+        bookingClient = mock(BookingClient.class);
+        when(studentRepository
+                .selectExistsEmail(newStudent.getEmail()))
+                .thenReturn(false);
+        when(chuckClient
+                .getJoke())
+                .thenReturn(new ChuckResponse(joke));
+        when(bookingClient
+                .createBooking(newStudent.getName()))
+                .thenReturn(id.intValue());
+        StudentService studentService = new StudentService(studentRepository, chuckClient, bookingClient);
+        //assert
+        assertDoesNotThrow(() -> studentService.addStudent(newStudent));
+    }
+
+    //Unit test
+    @Test
+    public void trowExceptionForAddExistedStudent() {
+        //arrange
+        String joke = "";
+        Long id = 1L;
+        Student newStudent = new Student(id, "Gnome", "javaGnome@mail.com", Gender.OTHER, "", 0);
+        studentRepository = mock(StudentRepository.class);
+        when(studentRepository
+                .selectExistsEmail(newStudent.getEmail()))
+                .thenReturn(true);
+        StudentService studentService = new StudentService(studentRepository, null, null);
+        //assert
+        assertThrows(BadRequestException.class, () -> studentService.addStudent(newStudent));
+    }
+
+    //Unit test
+    @Test
+    public void deleteExistedStudent() {
+        //arrange
+        Long id = 1L;
+        Student expectedStudent = new Student(id, "Gnome", "javaGnome@mail.com", Gender.OTHER, "", 0);
+        studentRepository = mock(StudentRepository.class);
+        when(studentRepository
+                .existsById(id))
+                .thenReturn(true);
+        StudentService studentService = new StudentService(studentRepository, null, null);
+        //assert
+        assertDoesNotThrow(() -> studentService.deleteStudent(id));
+    }
+
+    //Unit test
+    @Test
+    public void throwExceptionForDeleteNotExistedStudent() {
+        //arrange
+        Long id = 1L;
+        Student expectedStudent = new Student(id, "Gnome", "javaGnome@mail.com", Gender.OTHER, "", 0);
+        studentRepository = mock(StudentRepository.class);
+        when(studentRepository
+                .existsById(id))
+                .thenReturn(false);
+        StudentService studentService = new StudentService(studentRepository, null, null);
+        //assert
+        assertThrows(StudentNotFoundException.class, () -> studentService.deleteStudent(id));
     }
 }
 
